3ff6cb936c45ebd952ec7fc5159c281d6faf478c
JackHatlestad <Jackh19@icloud.com>
2025-06-28 15:25:06 -0500

    update

--- game.py
+++ game.py
@@ -1,54 +1,93 @@
-import pandas as pd
+import pandas as pd 
 import random
+import time
 
-team_name = input("Welcome to the NBA, What is your expansion teams name?")
+def load_existing():
+    team_name = input("What is your team name?\n")
 
-try:
-    df = pd.read_excel(f"{team_name}.xlsx")
-except FileNotFoundError:
-    df = pd.DataFrame([[2025,0,0,0,0,0,0,0,0,0,0,0,0,0]],columns = ['Year','Games Won', 'Games Lost', 'First Round', 'Confrence Semi-Finals', 
-                                                        'Conference Finals', 'NBA Finals', 'Champions', 'PG', 'SG', 'SF', 'PF', 
-                                                        'C', '6M'])
-    point_guard = random.randint(0,10)
-    shooting_guard = random.randint(0,10)
-    small_foward = random.randint(0,10)
-    power_foward = random.randint(0,10)
-    center = random.randint(0,10)
-    six_man = random.randint(0,10)
+    try:
+        df = pd.read_excel(f"{team_name}.xlsx")
+    except FileNotFoundError:
+        print("Error Can't Find Team")
+    return df, team_name
 
-    print("Let's go to the expansion draft.")
-    print(f"In the First Round the {team_name} select a {point_guard} overall Point Guard!")
-    print(f"In the second Round the {team_name} select a {shooting_guard} overall Shooting Guard!")
-    print(f"In the third Round the {team_name} select a {small_foward} overall Small foward!")
-    print(f"In the fourth Round the {team_name} select a {power_foward} overall power foward!")
-    print(f"In the five Round the {team_name} select a {center} overall Center!")
-    print(f"In the sixth Round the {team_name} select a {six_man} overall Sixth Man!")
+def create_new():
+    team_name = input("What is your team name?\n")
+    df = pd.DataFrame([[2025,0,0,0,0,0,0,0,0,0,0,0,0]], columns=['Year','Games Won', 'Games Lost', 'First Round',
+                                                                 'Confrence Semi-Finals', 'Confrence Finals', 
+                                                                'NBA Finals', 'Champions', 'PG', 'SG', 'SF', 'PF', 'C'])
+    print(f"Welcome {team_name}! Now it's time for the 2025 NBA Expansion Draft!")
+    time.sleep(5)
+    pg = random.randint(1,10)
+    df.at[0, 'PG'] = pg
+    print(f"In the first round of the 2025 NBA Expansion Draft the {team_name} select a {pg} overall Point Guard!")
+    time.sleep(5)
+    sg = random.randint(1,10)
+    df.at[0, 'SG'] = sg
+    print(f"In the first round of the 2025 NBA Expansion Draft the {team_name} select a {sg} overall Shooting Guard!")
+    time.sleep(5)
+    sf = random.randint(1,10)
+    df.at[0, 'SF'] = sf
+    print(f"In the first round of the 2025 NBA Expansion Draft the {team_name} select a {sf} overall Small Forward!")
+    time.sleep(5)
+    pf = random.randint(1,10)
+    df.at[0, 'PF'] = pg
+    print(f"In the first round of the 2025 NBA Expansion Draft the {team_name} select a {pf} overall Power Forward")
+    time.sleep(5)
+    c = random.randint(1,10)
+    df.at[0, 'C'] = c
+    print(f"In the first round of the 2025 NBA Expansion Draft the {team_name} select a {c} overall Center!")
+    time.sleep(5)
+    print("That concludes the 2025 NBA Expansion Draft!")
+    return df,team_name,pg,sg,sf,pf,c
 
-team_overall = point_guard + shooting_guard + small_foward + power_foward + center + six_man   
+def season(df):
+    team_overall = df.iloc[-1]['PG'] + df.iloc[-1]['SG'] + df.iloc[-1]['SF'] + df.iloc[-1]['PF'] + df.iloc[-1]['C']
 
-print(f"Welcome to the season")
-for i in range(82):
-    opponent_overall = random.randint(0,10) + random.randint(0,10)  + random.randint(0,10)  + random.randint(0,10) + random.randint(0,10) + random.randint(0,10) 
-    if team_overall > opponent_overall: 
-        print("Congrats you win!")
-        df['Games Won'] = df['Games Won'] + 1
+    for i in range(1,82):
+        opponent_overall = random.randint(5,50)
+        if team_overall > opponent_overall:
+            print(f"Congrats you won the game {i}!")
+            df.iloc[-1]['Games Won'] = df.iloc[-1]['Games Won'] + 1
+        else:
+            print(f"Sorry you lost game {i}")
+            df.iloc[-1]['Games Lost'] = df.iloc[-1]['Games Lost'] + 1
+    
+    Percentage = df.iloc[-1]['Games Won'] / 82
+
+    if Percentage > 0.80:
+        seed = random.randint(1,2)
+        print(f"Congrats you've made the playoffs as a {seed} seed")
+    elif Percentage > 0.60:
+        seed = random.randint(3,6)
+        print(f"Congrats you've made the playoffs as a {seed} seed")
+    elif Percentage > 0.50:
+        seed = random.randint(7,8)
+        print(f"Congrats you've made the playoffs as a {seed} seed")
     else:
-        print("Sorry you lost!")
-        df['Games Lost'] = df['Games Lost'] + 1
+        print("You missed the playoffs!")
+        return df
 
-if df.loc[0,'Games Won'] > 41:
-     print("Congrats you made the playoffs!")
-     
-
-
-
-
+def main():
+    choose_team = int(input("Welcome to the NBA!\n 1. Load Existing Team\n 2. Create Expansion Team\n"))
+    if choose_team == 1:
+        df,team_name = load_existing()
+    elif choose_team == 2:
+        df,team_name,pg,sg,sf,pf,c = create_new()
     
+    while True:
+        choose_mode = int(input("Do you want to:\n 1. Play a season\n 2. View Stats\n 3. Exit\n"))
+        if choose_mode == 1:
+            season(df)
+            print("Let's Play")
+        elif choose_mode == 2:
+            print(df[['Year','Games Won', 'Games Lost', 'First Round', 'Confrence Semi-Finals', 'Confrence Finals','NBA Finals', 
+                      'Champions' ]])
+        elif choose_mode == 3:
+            print("Thanks for playing!")
+            break
+        else:
+            print("Error")
 
-    
-    
-    
-
-                                                        
-                                                        
-                                                        
+if __name__ == "__main__":
+    main()
