25afe091867a664e6afc47be69278dc59e139f98
Jack Hatlestad <jackh19@icloud.com>
2025-06-29 17:32:33 -0500

    Added Free Agency

--- game.py
+++ game.py
@@ -24,19 +24,19 @@ def create_new():
     time.sleep(5)
     sg = random.randint(1,6)
     df.at[0, 'SG'] = sg
-    print(f"In the first round of the 2025 NBA Expansion Draft the {team_name} select a {sg} overall Shooting Guard!")
+    print(f"In the second round of the 2025 NBA Expansion Draft the {team_name} select a {sg} overall Shooting Guard!")
     time.sleep(5)
     sf = random.randint(1,6)
     df.at[0, 'SF'] = sf
-    print(f"In the first round of the 2025 NBA Expansion Draft the {team_name} select a {sf} overall Small Forward!")
+    print(f"In the third round of the 2025 NBA Expansion Draft the {team_name} select a {sf} overall Small Forward!")
     time.sleep(5)
     pf = random.randint(1,6)
     df.at[0, 'PF'] = pg
-    print(f"In the first round of the 2025 NBA Expansion Draft the {team_name} select a {pf} overall Power Forward")
+    print(f"In the four round of the 2025 NBA Expansion Draft the {team_name} select a {pf} overall Power Forward")
     time.sleep(5)
     c = random.randint(1,6)
     df.at[0, 'C'] = c
-    print(f"In the first round of the 2025 NBA Expansion Draft the {team_name} select a {c} overall Center!")
+    print(f"In the fifth round of the 2025 NBA Expansion Draft the {team_name} select a {c} overall Center!")
     time.sleep(5)
     print("That concludes the 2025 NBA Expansion Draft!")
     return df,team_name,pg,sg,sf,pf,c
@@ -45,9 +45,9 @@ def offseason(df):
     last_row = df.iloc[-1]
     pg, sg, sf, pf, c = last_row['PG'], last_row['SG'], last_row['SF'], last_row['PF'], last_row['C']
     print(f"Welcome to the {last_row['Year']} Offseaon!")
-    time.sleep(1)
+    time.sleep(5)
     print(f"Your current roster: \n 1. PG: {pg}\n2. SG: {sg}\n 3. SF: {sf}\n 4. PF: {pf} \n 5. C : {c}")
-    time.sleep(1)
+    time.sleep(5)
     draft_choice = input("Welcome to the NBA Draft! What position do you want to draft?")
     new_value = random.randint(1, 10)
     
@@ -68,14 +68,182 @@ def offseason(df):
         c = new_value
     else:
         print("Invalid choice. No upgrade this offseason.")
+    time.sleep(1)
+    print("Welcome to NBA Free Agency")
+    time.sleep(1)
+    chance = random.randint(1,10)
+    pg_chance = random.randint(1,10)
+    sg_chance = random.randint(1,10)
+    pf_chance = random.randint(1,10)
+    sf_chance = random.randint(1,10)
+    c_chance = random.randint(1,10)
+    if chance == pg_chance:
+        print("Sorry your Point Guard Left in Free Agency!")
+        time.sleep(1)
+        new_rating = random.randint(1,10)
+        pg = new_rating
+        print(f"Your new Point Guard is a {pg} overall!")
+    if chance == sg_chance:
+        print("Sorry your Shooting Guard Left in Free Agency!")
+        time.sleep(1)
+        new_rating = random.randint(1,10)
+        sg = new_rating
+        print(f"Your new Shooting Guard is a {sg} overall!")
+    if chance == pf_chance:
+        print("Sorry your Power Foward Left in Free Agency!")
+        time.sleep(1)
+        new_rating = random.randint(1,10)
+        pf = new_rating
+        print(f"Your new Power Foward is a {pf} overall!")
+    if chance == sf_chance:
+        print("Sorry your Small Foward Left in Free Agency!")
+        time.sleep(1)
+        new_rating = random.randint(1,10)
+        sf = new_rating
+        print(f"Your new Small Foward is a {sf} overall!")
+    if chance == c_chance:
+        print("Sorry your Center Left in Free Agency!")
+        time.sleep(1)
+        new_rating = random.randint(1,10)
+        c = new_rating
+        print(f"Your new Center is a {c} overall!")
 
     return pg, sg, sf, pf, c
 
-def playoffs(new_row,seed, team_overall):
+def NBA_Finals(new_row):
+    new_row['NBA Finals'] = 1
+    player_wins = 0
+    computer_wins = 0
+
+    while True:
+        time.sleep(5)
+        computer_choice = random.randint(0,2)
+        player_choice = input("What is your choice?\n")
+
+        if player_choice.lower() == 'rock':
+            if computer_choice == 0:
+                print("The computer choose rock, it's a tie.")
+                print(f"The series is {player_wins} to {computer_wins}!")
+
+            elif computer_choice == 1:
+                print("The computer choose paper, you lose!")
+                computer_wins = computer_wins + 1
+                print(f"The series is {player_wins} to {computer_wins}!")
+            elif computer_choice == 2:
+                print("The computer choose scissors, you win!")
+                player_wins = player_wins + 1
+                print(f"The series is {player_wins} to {computer_wins}!")
+
+        elif player_choice.lower() == 'paper':
+            if computer_choice == 0:
+                print("The computer choose rock, you win!")
+                player_wins = player_wins + 1
+                print(f"The series is {player_wins} to {computer_wins}!")
+            elif computer_choice == 1:
+                print("The computer choose paper, it's a tie.")
+                print(f"The series is {player_wins} to {computer_wins}!")
+            elif computer_choice == 2:
+                print("The computer choose scissors, you lose!")
+                computer_wins = computer_wins + 1
+                print(f"The series is {player_wins} to {computer_wins}!")
+
+        elif player_choice.lower() == 'scissors':
+            if computer_choice == 0:
+                print("The computer choose rock, you lose!")
+                computer_wins = computer_wins + 1
+                print(f"The series is {player_wins} to {computer_wins}!")
+
+            elif computer_choice == 1:
+                print("The computer choose paper, you win!")
+                player_wins = player_wins + 1
+                print(f"The series is {player_wins} to {computer_wins}!")
+
+            elif computer_choice == 2:
+                print("The computer choose scissors, it's a tie.")
+                print(f"The series is {player_wins} to {computer_wins}!")
+        else:
+            print("Error please choose Rock, Paper, or Scissors")
+    
+        if player_wins == 4:
+            print("Congratulations" + "! You won the NBA Finals!") 
+            new_row['Champions'] = 1
+            break
+            
+        if computer_wins == 4:
+            print("Sorry you lost the NBA Finals!")
+            break
+
+def Confrence_Finals(new_row,team_overall):
+    new_row['Confrence Finals'] = 1
+    print(f"Welcome to the Confrence Finals!")
+    player_wins = 0
+    opponent_wins = 0
+    while True:
+        time.sleep(5)
+        opponent_overall = random.randint(35,50)
+        if team_overall > opponent_overall:
+            print(f"Congrats you won game of the First Round!")
+            player_wins = player_wins + 1
+        else:
+            print(f"Sorry you lost game  of the First Round!")
+            opponent_wins = opponent_wins + 1
+        
+        if player_wins == 4:
+            print("Congrats you won the series and advance to Confrence Fiansl")
+            NBA_Finals(new_row)
+            break
+        if opponent_wins == 4:
+            print("Sorry you lost the series")
+            break
+
+def Semi_Finals(new_row, team_overall):
+    new_row['Confrence Semi-Finals'] = 1
+    print(f"Welcome to the Confrence Semi Finals!")
+    player_wins = 0
+    opponent_wins = 0
+    while True:
+        time.sleep(5)
+        opponent_overall = random.randint(30,50)
+        if team_overall > opponent_overall:
+            print(f"Congrats you won game of the First Round!")
+            player_wins = player_wins + 1
+        else:
+            print(f"Sorry you lost game  of the First Round!")
+            opponent_wins = opponent_wins + 1
+        
+        if player_wins == 4:
+            print("Congrats you won the series and advance to Confrence Fiansl")
+            Confrence_Finals(new_row, team_overall)
+            break
+        if opponent_wins == 4:
+            print("Sorry you lost the series")
+            break
+
+    print("Congrats")
+
+def first_round(new_row,seed, team_overall):
     print(f"Welcome to the {new_row['Year']} playoffs")
     new_row['First Round'] = 1
-    print("Welcome to the first round")
-
+    player_wins = 0
+    opponent_wins = 0
+    while True:
+        time.sleep(5)
+        opponent_overall = random.randint(20,50)
+        if team_overall > opponent_overall:
+            print(f"Congrats you won game of the First Round!")
+            player_wins = player_wins + 1
+        else:
+            print(f"Sorry you lost game  of the First Round!")
+            opponent_wins = opponent_wins + 1
+        
+        if player_wins == 4:
+            print("Congrats you won the series and advance to Confrence Semi Fiansl")
+            Semi_Finals(new_row, team_overall)
+            break
+        if opponent_wins == 4:
+            print("Sorry you lost the series")
+            break
+        
 def season(df,pg,sg,sf,pf,c):
     new_row = {
         'Year': df.iloc[-1]['Year'] + 1,
@@ -95,7 +263,7 @@ def season(df,pg,sg,sf,pf,c):
     team_overall = new_row['PG'] + new_row['SG'] + new_row['PF'] + new_row['SF'] + new_row['C']
     
 
-    for i in range(1,82):
+    for i in range(1,83):
         opponent_overall = random.randint(5,50)
         if team_overall > opponent_overall:
             print(f"Congrats you won the game {i}!")
@@ -110,15 +278,15 @@ def season(df,pg,sg,sf,pf,c):
     if Percentage > 0.80:
         seed = random.randint(1,2)
         print(f"Congrats you've made the playoffs as a {seed} seed")
-        playoffs(new_row,seed, team_overall)
+        first_round(new_row,seed, team_overall)
     elif Percentage > 0.60:
         seed = random.randint(3,6)
         print(f"Congrats you've made the playoffs as a {seed} seed")
-        playoffs(new_row,seed, team_overall)
+        first_round(new_row,seed, team_overall)
     elif Percentage > 0.50:
         seed = random.randint(7,8)
         print(f"Congrats you've made the playoffs as a {seed} seed")
-        playoffs(new_row,seed, team_overall)
+        first_round(new_row,seed, team_overall)
     else:
         print("You missed the playoffs!")
         
@@ -142,6 +310,7 @@ def main():
                       'Champions']])
         elif choose_mode == 3:
             print("Thanks for playing!")
+            df.to_excel(f"{team_name}.xlsx", index=False)
             break
         else:
             print("Error")
